3a424dd359ff89f7a6b2cf8b3049a7bd42f50e99
TijanaTurner <nonkovictijana@gmail.com>
2023-03-17 11:53:37 -0400

    update

--- src/test/java/com/project_name/step_definitions/US104.java
+++ src/test/java/com/project_name/step_definitions/US104.java
@@ -1,13 +1,22 @@
 package com.project_name.step_definitions;
 
 import com.project_name.pages.SalesManagerPage;
+import com.project_name.pages.SalesPage;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 
 public class US104 {
 
     SalesManagerPage sales_Manager_Page = new SalesManagerPage();
+    SalesPage salesPage= new SalesPage();
 
+    @Given("verify user is logged as sales manager")
+    public void verifyUserIsLoggedAsSalesManager() {
+        salesPage.login("salesmanager15@info.com", "salesmanager");
+        Assert.assertTrue(salesPage.LoginNameButton.isDisplayed());
+
+    }
 
     @Then("verify the sales manager has access to {int} modules")
     public void verify_the_sales_manager_has_access_to_modules(Integer expectedNumberOfModules) {
@@ -15,5 +24,6 @@ public class US104 {
         Assert.assertEquals(expectedNumberOfModules, Integer.valueOf(sales_Manager_Page.mainModules.size()));
 
     }
-
 }
+
+
--- src/test/resources/features/US104.feature
+++ src/test/resources/features/US104.feature
@@ -1,8 +1,7 @@
+
 Feature: As a sales manager, I should be able to access the different modules
 
 
   Scenario:
-    Then verify user is logged as sales manager
-
-  Scenario:
+    Given verify user is logged as sales manager
     Then verify the sales manager has access to 19 modules
